e8fe689c9b26e6c04c0af000c95d1c7076b9e745
Litter Brother Xiao <96779545+LitterBrother-Xiao@users.noreply.github.com>
2024-08-09 23:52:43 +0800

    Update process_data.sh

--- scripts/process/process_data.sh
+++ scripts/process/process_data.sh
@@ -2,7 +2,7 @@ python ../..//tools/preprocess_data_gebert.py \
     --json-file xxx.jsonl \
     --json-key 'text' \
     --group-size 2048 \
-    --tokenizer-model /nvme/xys/models/qwen \
+    --tokenizer-model xxx \
     --tokenizer-type "HFTokenizer" \
     --output-prefix xxx \
     --dataset-impl mmap \
